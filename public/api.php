a32e25a588c41977956664829a050e7eeb1a3a4d
Andrew Forward <aforward@gmail.com>
2020-04-02 09:30:37 -0400

    Allow to return XML or JSON
    Using the X-Reply-Type,
    a client can specify if they want `xml` or `json`.
    
    Default to `json`.

--- public/api.php
+++ public/api.php
@@ -1,9 +1,43 @@
 <?php
 
-$data = [
+$jokes = [
+  [
     "question" => "What do you call a cow with no legs?",
     "answer" => "Ground beef.",
+  ],
+  [
+    "question" => "Why did the chicken cross the road?",
+    "answer" => "Because I didn't think up any other jokes before the lecture.",
+  ]
 ];
 
-header('Content-Type: application/json');
-echo json_encode($data);
+$jokeIndex = array_rand($jokes, 1);
+$joke = $jokes[$jokeIndex];
+
+$headers = getallheaders();
+
+if (isset($headers["X-Reply-Type"])) {
+  $replyType = $headers["X-Reply-Type"];
+} else {
+  $replyType = null;
+}
+
+switch(strtolower($replyType)) {
+case "xml":
+case "application/xml":
+  header("Content-Type: text/xml");
+  $xmlOutput = "
+    <joke>
+      <question>{$joke["question"]}</question>
+      <answer>{$joke["answer"]}</answer>
+    </joke>
+  ";
+  echo trim($xmlOutput);
+  break;
+case "json":
+case "application/json":
+default:
+  header("Content-Type: application/json");
+  echo json_encode($joke);
+  break;
+}
